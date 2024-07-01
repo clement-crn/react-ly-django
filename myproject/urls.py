409278e322356f3632fbf2780b7aa9fbc9fdf605
Clément <clement@waven-consulting.fr>
2024-07-01 19:32:31 +0200

    api urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include # 👈 Add include here
+from django.urls import path, include 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from myapp import views
+from myapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/home/', views.HomeView.as_view(), name ='home'),
+    path('api/logout/', views.LogoutView.as_view(), name ='logout')
 ]
